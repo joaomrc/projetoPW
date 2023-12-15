58473b6c11eb6349e4d5f71bf665991746092b3f
joão <120732181+joaomrc@users.noreply.github.com>
2023-12-15 15:05:59 +0000

    Add files via upload

---
+++ projetoPW/README.md
@@ -0,0 +1,22 @@
+# Projeto | Programação para Web 2
+
+## Objetivo 
+<table>
+<tr>
+<td>
+Avaliação de cidades
+</td>
+</tr>
+</table>
+
+## Autor
+<table>
+<tr>
+<td>
+João Coelho
+</td>
+</tr>
+</table>
+
+### Prazo de Entrega
+janeiro de 2024
---
+++ projetoPW/view/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avaliação de Cidades</title>
+    <!-- Adicione seus estilos CSS aqui -->
+</head>
+<body>
+    <h1>Avaliação de Cidades</h1>
+    
+    <!-- Exiba as cidades e formulários de avaliação aqui -->
+
+    <!-- Adicione seus scripts JavaScript aqui -->
+</body>
+</html>
